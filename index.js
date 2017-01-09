38ef6320b771026a895fefe16a8534f0c2e8942a
Leo Lamprecht <mindrun@icloud.com>
2017-01-09 22:15:37 +0100

    Lint using XO

--- index.js
+++ index.js
@@ -13,41 +13,43 @@ module.exports = async (req, res) => {
 cacheData()
 setInterval(cacheData, ms('15m'))
 
-function log (text) {
+function log(text) {
   return slack(text, 'T0CAQ00TU/B3BTWAJLR/Lyo5DjtFTFILfRPfUAa5yLoQ')
 }
 
-function logError (text) {
+function logError(text) {
   return slack(text, 'T0CAQ00TU/B3D9078A3/hlmwh17yhjGztTKmBzCQG7lV')
 }
 
-function slack (text, id) {
+function slack(text, id) {
   fetch(`https://hooks.slack.com/services/${id}`, {
     method: 'POST',
-    body: JSON.stringify({ text })
+    body: JSON.stringify({text})
   })
 }
 
-function cacheData () {
+function cacheData() {
   const start = Date.now()
   fetch('https://api.github.com/orgs/zeit/repos', {
     headers: {
       Accept: 'application/vnd.github.preview'
     }
   })
-  .then((res) => {
-    if (200 !== res.status) {
+  .then(res => {
+    if (res.status !== 200) {
       return logError('Non-200 response code from GitHub: ' + res.status)
     }
     return res.json()
   })
-  .then((data_) => {
-    if (!data_) return
+  .then(data_ => {
+    if (!data_) {
+      return
+    }
 
     // ugly hack because github sometimes doesn't return
     // all the right search results :|
     let featured = 0
-    data_.forEach(({ name }) => {
+    data_.forEach(({name}) => {
       if (name === 'hyper' || name === 'next.js' || name === 'micro') {
         featured++
       }
@@ -57,7 +59,7 @@ function cacheData () {
       return logError(`Error: GitHub did not include all projects (${featured})`)
     }
 
-    data = data_.map(({ name, description, stargazers_count, html_url }) => ({
+    data = data_.map(({name, description, stargazers_count, html_url}) => ({
       name,
       description,
       url: html_url,
@@ -68,9 +70,9 @@ function cacheData () {
 
     log(`Re-built projects cache. ` +
         `Total: ${data.length} public ▲ZEIT projects. ` +
-        `Elapsed: ${(new Date - start)}ms`)
+        `Elapsed: ${(new Date() - start)}ms`)
   })
-  .catch((err) => {
+  .catch(err => {
     logError('Error parsing response from GitHub: ' + err.stack)
   })
 }
