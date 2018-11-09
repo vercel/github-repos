c23291f71582995d1872f4fbb3c6ade152573de1
Leo Lamprecht <mindrun@icloud.com>
2018-11-09 16:19:46 +0100

    Fixed it all

--- index.js
+++ index.js
@@ -1,79 +1,83 @@
-const fetch = require('node-fetch')
-const ms = require('ms')
+const fetch = require('node-fetch');
+const ms = require('ms');
 
-let data = []
+let cache = null;
 
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
-  return data
-}
-
-// Cache data now and every X ms
-cacheData()
-setInterval(cacheData, ms('15m'))
-
-function log(text) {
-  return slack(text, process.env.TOKEN_EVENTS)
-}
-
-function logError(text) {
-  return slack(text, process.env.TOKEN_ALERTS)
-}
-
-function slack(text, id) {
+const slack = (text, id) => {
   fetch(`https://hooks.slack.com/services/${id}`, {
     method: 'POST',
-    body: JSON.stringify({text})
-  })
-}
+    body: JSON.stringify({ text })
+  });
+};
 
-function cacheData() {
-  const start = Date.now()
-  fetch('https://api.github.com/orgs/zeit/repos?per_page=100', {
-    headers: {
-      Accept: 'application/vnd.github.preview'
-    }
-  })
-  .then(res => {
-    if (res.status !== 200) {
-      return logError('Non-200 response code from GitHub: ' + res.status)
-    }
-    return res.json()
-  })
-  .then(data_ => {
-    if (!data_) {
-      return
-    }
+const log = text => slack(text, process.env.TOKEN_EVENTS);
+const logError = text => slack(text, process.env.TOKEN_ALERTS);
 
-    // Ugly hack because github sometimes doesn't return
-    // all the right search results :|
-    let featured = 0
-    data_.forEach(({name}) => {
-      if (name === 'hyper' || name === 'next.js' || name === 'micro') {
-        featured++
+const getData = async () => {
+  const start = Date.now();
+
+  const response = fetch(
+    'https://api.github.com/orgs/zeit/repos?per_page=100',
+    {
+      headers: {
+        Accept: 'application/vnd.github.preview'
       }
-    })
-
-    if (featured !== 3) {
-      return logError(`Error: GitHub did not include all projects (${featured})`)
     }
+  );
 
-    data = data_
-      .filter(repo => !repo.archived)
-      .map(({name, description, stargazers_count, html_url}) => ({
-        name,
-        description,
-        url: html_url,
-        stars: stargazers_count
-      }))
-      .sort((p1, p2) => p2.stars - p1.stars)
+  if (response.status !== 200) {
+    logError(`Non-200 response code from GitHub: ${response.status}`);
+    return null;
+  }
 
-    log(`Re-built projects cache. ` +
-        `Total: ${data.length} public ▲ZEIT projects. ` +
-        `Elapsed: ${(new Date() - start)}ms`)
-  })
-  .catch(err => {
-    logError('Error parsing response from GitHub: ' + err.stack)
-  })
-}
+  const parsed = await response.json();
+
+  // Ugly hack because github sometimes doesn't return
+  // all the right search results :|
+  let featured = 0;
+
+  parsed.forEach(({ name }) => {
+    if (name === 'hyper' || name === 'next.js' || name === 'micro') {
+      featured += 1;
+    }
+  });
+
+  if (featured !== 3) {
+    logError(`Error: GitHub did not include all projects (${featured})`);
+    return null;
+  }
+
+  /* eslint-disable camelcase */
+  const final = parsed
+    .filter(repo => !repo.archived)
+    .map(({ name, description, stargazers_count, html_url }) => ({
+      name,
+      description,
+      url: html_url,
+      stars: stargazers_count
+    }))
+    .sort((p1, p2) => p2.stars - p1.stars);
+  /* eslint-enable camelcase */
+
+  log(
+    `Re-built projects cache. ` +
+      `Total: ${final.length} public ZEIT projects. ` +
+      `Elapsed: ${new Date() - start}ms`
+  );
+
+  return {
+    list: final,
+    lastUpdate: Date.now()
+  };
+};
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  if (!cache || Date.now() - cache.lastUpdate > ms('5m')) {
+    cache = await getData();
+  }
+
+  res.end(JSON.stringify(cache.list));
+};
